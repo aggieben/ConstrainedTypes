3a6e0511446a2883fb689c249d5d3497b056e38c
Benjamin Collins <aggieben@gmail.com>
2020-06-22 11:20:21 -0500

    Delete everything

--- src/ConstrainedTypes/test.fsx
+++
@@ -1,9 +0,0 @@
-#r "nuget: FSharp.Compiler.Service"
-#r "./bin/Debug/netstandard2.1/ConstrainedTypes.dll"
-
-
-open ConstrainedTypes
-type T = ConstrainedString<"fun (s:string) -> not (System.String.IsNullOrWhiteSpace(s))">
-
-printfn ""
-
